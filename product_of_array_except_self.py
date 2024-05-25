5abc16aefb7ed060855d474d512f0b23f4daf0a7
Ruslan Serebriakov <rsrbk@users.noreply.github.com>
2024-05-25 16:47:07 +0200

    Fix time complexity to O(n) by optimizing the algorithm.

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -1,12 +1,18 @@
 def product_except_self(nums):
     n = len(nums)
     result = [1] * n
+    left_product = [1] * n
+    right_product = [1] * n
+    
+    for i in range(1, n):
+        left_product[i] = left_product[i - 1] * nums[i - 1]
+    
+    for i in range(n - 2, -1, -1):
+        right_product[i] = right_product[i + 1] * nums[i + 1]
     
     for i in range(n):
-        for j in range(n):
-            if i != j:
-                result[i] *= nums[j]
-                
+        result[i] = left_product[i] * right_product[i]
+    
     return result
 
 # Test the function with the example input
